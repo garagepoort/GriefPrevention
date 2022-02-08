ee2941fa5181a6ad1e10ace7d4492fa6e9470d9d
David Maes <unconfigured@null.spigotmc.org>
2022-02-08 02:11:16 +0100

    refactoring part 1

---
+++ src/main/java/me/ryanhamshire/GriefPrevention/cmd/PermissionTrustCmd.java
@@ -0,0 +1,30 @@
+package me.ryanhamshire.GriefPrevention.cmd;
+
+import be.garagepoort.mcioc.IocBean;
+import be.garagepoort.mcioc.IocCommandHandler;
+import me.ryanhamshire.GriefPrevention.TrustService;
+import me.ryanhamshire.GriefPrevention.util.BukkitUtils;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@IocBean
+@IocCommandHandler("permissiontrust")
+public class PermissionTrustCmd extends AbstractCmd {
+    private final TrustService trustService;
+    private final BukkitUtils bukkitUtils;
+
+    public PermissionTrustCmd(TrustService trustService, BukkitUtils bukkitUtils) {
+        this.trustService = trustService;
+        this.bukkitUtils = bukkitUtils;
+    }
+
+    @Override
+    protected boolean executeCmd(CommandSender sender, String alias, String[] args) {
+        validateIsPlayer(sender);
+        Player player = (Player) sender;
+        if (args.length != 1) return false;
+        bukkitUtils.runTaskAsync(sender, () -> trustService.handleTrust(player, null, args[0]));
+
+        return true;
+    }
+}
